c51de0b9ca8d11e7452c8e805d3b93f43d57132f
kjaveed <javeekh@ssfs.org>
2022-01-11 11:22:52 -0500

    stuff

--- assignment_nine.py
+++ assignment_nine.py
@@ -1,14 +1,21 @@
 import card
 import deck
+
+
 def deal_cards(the_deck):
     hand = []
     for x in range(5):
         hand.append(the_deck.deal())
     return hand
+
+
 def compare_cards(card1, card2):
-    card1 = deal_cards()
-    card2 = deal_cards()
     if card1 > card2:
-        print("Player One wins the round")
+        return "Player One"
     elif card2 > card1:
-        print("player two wins the round")
+        return "Player Two"
+
+
+def main():
+    player_one = deal_cards()
+    player_two = deal_cards()
